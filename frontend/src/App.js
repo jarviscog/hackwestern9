1b02a294a49801cf3a67bf78c6378b6d43f6d0c2
William-04 <William177013@gmail.com>
2022-11-19 11:28:30 -0500

    Changes

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,25 +1,45 @@
-import logo from './logo.svg';
+
+import React, { useState } from "react";
 import './App.css';
 
-function App() {
+function Alert () { 
+    alert("Clicked") //Alert user
+  }
+  
+const view = {
+    blue: {
+      default: "#8C51FF",
+      hover: "##5e28c7" },
+  };
+  
+function StartBtn(props) { 
+       return <button onClick= {Alert}> {props.text} </button>
+}
+
+
+
+const App = () => {
+   
+  const name = ""
+  const issomeone = true; 
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div  className="App" >
+        
+        
+        <h1> Hello, Welcome to SportzML{issomeone ? name : 'someone'}</h1>
+        <StartBtn text = "Click"/> 
+        <div className = "search"> 
+        <input placeholder = "Search teams" /> 
+        </div>
     </div>
+    
   );
 }
+
+
+
+
+
 
 export default App;
